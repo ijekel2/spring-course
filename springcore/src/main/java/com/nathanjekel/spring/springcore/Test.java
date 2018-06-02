b878b34fc196c51affb8c102a6af3d43f7684cab
nathanjekel <ijekel2@gmail.com>
2018-06-02 10:41:25 -0400

    first commmit for spring core project

---
+++ springcore/src/main/java/com/nathanjekel/spring/springcore/Test.java
@@ -0,0 +1,16 @@
+package com.nathanjekel.spring.springcore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		// Searches for XML config file on the ClassPath
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/nathanjekel/spring/springcore/config.xml");
+		Employee emp = (Employee) context.getBean("emp");
+		System.out.println("Employee ID: " + emp.getId());
+		System.out.println("Employee Name: " + emp.getName());
+	}
+
+}
