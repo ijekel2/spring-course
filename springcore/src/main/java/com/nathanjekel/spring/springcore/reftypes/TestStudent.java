ba0b6644779b58c2b87d3c91e63c9d65ea516445
nathanjekel <ijekel2@gmail.com>
2018-06-05 17:51:06 -0400

    adding properties file

--- springcore/src/main/java/com/nathanjekel/spring/springcore/reftypes/TestStudent.java
+++ springcore/src/main/java/com/nathanjekel/spring/springcore/reftypes/TestStudent.java
@@ -3,9 +3,10 @@ package com.nathanjekel.spring.springcore.reftypes;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestStudent {
-	
+
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/nathanjekel/spring/springcore/reftypes/reftypesconfig.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/nathanjekel/spring/springcore/reftypes/reftypesconfig.xml");
 		Student student = (Student) context.getBean("student");
 		System.out.println(student);
 		context.close();
