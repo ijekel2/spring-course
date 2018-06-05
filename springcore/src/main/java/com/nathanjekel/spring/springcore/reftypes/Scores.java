7829f89858208792f5b753c99045087233888ac0
nathanjekel <ijekel2@gmail.com>
2018-06-05 08:59:24 -0400

    setter injection with reference types

--- springcore/src/main/java/com/nathanjekel/spring/springcore/reftypes/Scores.java
+++ springcore/src/main/java/com/nathanjekel/spring/springcore/reftypes/Scores.java
@@ -2,4 +2,38 @@ package com.nathanjekel.spring.springcore.reftypes;
 
 public class Scores {
 
+	private Double math;
+	private Double physics;
+	private Double chemistry;
+
+	public double getMath() {
+		return math;
+	}
+
+	public void setMath(double math) {
+		this.math = math;
+	}
+
+	public double getPhysics() {
+		return physics;
+	}
+
+	public void setPhysics(double physics) {
+		this.physics = physics;
+	}
+
+	public double getChemistry() {
+		return chemistry;
+	}
+
+	public void setChemistry(double chemistry) {
+		this.chemistry = chemistry;
+	}
+
+	@Override
+	public String toString() {
+		return "Scores [math=" + math + ", physics=" + physics + ", chemistry=" + chemistry + "]";
+	}
+	
+
 }
--- springcore/src/main/java/com/nathanjekel/spring/springcore/reftypes/Student.java
+++ springcore/src/main/java/com/nathanjekel/spring/springcore/reftypes/Student.java
@@ -1,5 +1,20 @@
 package com.nathanjekel.spring.springcore.reftypes;
 
 public class Student {
+	private Scores scores;
 
+	public Scores getScores() {
+		return scores;
+	}
+
+	public void setScores(Scores scores) {
+		this.scores = scores;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [scores=" + scores + "]";
+	}
+	
+	
 }
